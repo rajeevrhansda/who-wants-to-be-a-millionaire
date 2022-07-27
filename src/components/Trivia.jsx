1c51b5a6bbeb63d2a621c422d1900e6bb8e0feb6
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-27 12:02:03 +0530

    use-sound

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,4 +1,10 @@
 import React, { useState, useEffect } from 'react'
+import useSound from 'use-sound';
+import play from "../assets/sounds/play.mp3"
+import correct from "../assets/sounds/correct.mp3"
+import wrong from "../assets/sounds/wrong.mp3"
+
+
 
 export default function Trivia({
     data,
@@ -9,6 +15,13 @@ export default function Trivia({
     const [question, setQuestion] = useState(null);
     const [selectedAnswer, setSelectedAnswer] = useState(null);
     const [className, setClassName] = useState("answer");
+    const [letsPlay] = useSound(play);
+    const [correctAnswer] = useSound(correct);
+    const [wrongAnswer] = useSound(wrong);
+
+    useEffect(() => {
+        letsPlay()
+    }, [letsPlay])
 
     useEffect(() => {
         setQuestion(data[questionNumber - 1]);
@@ -24,12 +37,18 @@ export default function Trivia({
         setSelectedAnswer(a);
         setClassName("answer active");
         delay(3000, () => setClassName(a.correct ? "answer correct" : "answer wrong"));
-        delay(6000, ()=> {
-            if(a.correct){
-                setQuestionNumber((prev)=> prev+1);
-                setSelectedAnswer(null);
-            }else{
-                setStop(true);
+        delay(5000, () => {
+            if (a.correct) {
+                correctAnswer();
+                delay(1000, () => {
+                    setQuestionNumber((prev) => prev + 1);
+                    setSelectedAnswer(null);
+                });
+            } else {
+                wrongAnswer();
+                delay(1000, () => {
+                    setStop(true);
+                })
             }
         })
 
